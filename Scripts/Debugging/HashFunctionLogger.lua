b3ac5f83d21185a19da3c328a71601f925b059b9
petewar3 <danieladams201178@gmail.com>
2026-01-01 04:10:34 +0000

    Update HashFunctionLogger.lua

--- Scripts/Debugging/HashFunctionLogger.lua
+++ Scripts/Debugging/HashFunctionLogger.lua
@@ -1,6 +1,6 @@
 game.Loaded:Wait()
 local target_hash = "293cf47e10f01f0fc030c1a79829e28cf7cda2629ffcb6d3295710dca2b71f0ba39acecee058d182cf14fe89bd765921"
--- hash number 14
+-- hash table number 14
 
 for _, func in pairs(getgc()) do
     if typeof(func) == "function" and not iscclosure(func) and getfunctionhash(func) == target_hash then
